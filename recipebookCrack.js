b6be6c365eb94f9594c9774b3c5096ccd199d25d
RuyeNet <31236859+RuyeNet@users.noreply.github.com>
2022-03-18 23:58:40 +0800

    Update recipebookCrack.js

--- recipebookCrack.js
+++ recipebookCrack.js
@@ -1,6 +1,6 @@
 /*
 https://apiios_dq.xiangha.com/v4/dish/info url script-response-body https://raw.githubusercontent.com/RuyeNet/QuantumultX/main/recipebookCrack.js
-hostname= *
+hostname = *
 */
 
 var str = $response.body;
